d7a32a86e01ca094295f3115ab2c212a097b16ec
Scott Anderson <frostlocke@gmail.com>
2019-10-15 20:53:23 -0600

    added new images/css

--- air_p_p/src/components/home.js
+++ air_p_p/src/components/home.js
@@ -82,9 +82,8 @@ function HomePage() {
               placeholder="Find the perfect potty"
             />
             <span class="input-group-btn"></span>
-            <button class="btn btn-secondary btn-lg src-button" type="submit">
-              {" "}
-              Search{" "}
+            <button class="toilet-button" src="./images/toilet-paper.png" /><button type="submit">
+              
             </button>
           </div>
         </div>
@@ -110,7 +109,7 @@ function HomePage() {
             <div class="col">
               <a href="top10.html" />
               <div class="Top">
-                <img class="indexImg" src="./images/top10.jpg" />
+                <img class="indexImg" src="./images/top10-900x400.jpg" />
                 <p> Top 10 </p>
               </div>
             </div>
