cd8ecbeb58734c98782ac33f29574ad5d642bc40
Scott Anderson <frostlocke@gmail.com>
2019-10-22 20:52:54 -0600

    got map data rendering

--- air_p_p/src/components/home.js
+++ air_p_p/src/components/home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./home.css";
-import SignIn from './signIn'
-import Review from './review'
-import Malls from './malls'
-import Schools from './schools'
-import PottyMap from './pottymap'
+import SignIn from "./signIn";
+import Review from "./review";
+import Malls from "./malls";
+import Schools from "./schools";
+import PottyMap from "./pottymap";
+import PottySearch from "./pottysearch";
 
 export default function App() {
   return (
@@ -34,27 +35,27 @@ export default function App() {
           </ul>
         </nav>
         <nav>
-        <Switch>
-          <Route path="/signIn">
-            <SignInRender />
-          </Route>
-          <Route path="/review">
-            <ReviewRender />
-          </Route>
-          <Route path="/malls">
-            <MallsRender />
-          </Route>
-          <Route path="/schools">
-            <Schools />
-          </Route>
-          <Route path="/Top10">
-            <Top10 />
-          </Route>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-        </Switch>
-      </nav>
+          <Switch>
+            <Route path="/signIn">
+              <SignInRender />
+            </Route>
+            <Route path="/review">
+              <ReviewRender />
+            </Route>
+            <Route path="/malls">
+              <MallsRender />
+            </Route>
+            <Route path="/schools">
+              <Schools />
+            </Route>
+            <Route path="/Top10">
+              <Top10 />
+            </Route>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+          </Switch>
+        </nav>
       </div>
     </Router>
   );
@@ -84,59 +85,65 @@ function Top10() {
   return <h2>Top 10</h2>;
 }
 
-function HomePage() {
-  return (
-    <div>
-      <br></br>
+class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <div class="jumbotron">
-        <div class="container">
-          <h3 class="display-4 airbnb"> AirPnP </h3>
-          <br></br>
-          <h3 class="display-4 air-descrip">Locate a restroom near you.</h3>
-          <br></br>
-          <div class="input-group">
-            <input
-              class="form-control"
-              type="text"
-              placeholder="Find the perfect potty"
+    this.state = {
+      userSearch: "10011 Centennial Parkway",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <br></br>
+
+        <div class="jumbotron">
+          <div class="container">
+            <h3 class="display-4 airbnb"> AirPnP </h3>
+            <br></br>
+            <h3 class="display-4 air-descrip">Locate a restroom near you.</h3>
+            <br></br>
+            <PottySearch
+              onSearch={(userSearchText) => {
+                this.setState({ userSearch: userSearchText });
+              }}
             />
-            <span class="input-group-btn"></span>
-            <img class="toilet-button" src="images/toilet-paper.png" />
+            <PottyMap userSearch={this.state.userSearch} />
           </div>
-          <PottyMap/>
         </div>
-      </div>
 
-      <div class="jumbotron">
-        <div class="container slider">
-          <div class="row">
-            <div class="col">
-              <a href="malls.html" />
-              <div class="malls">
-                <img class="indexImg" src="./images/mall.jpg" />
-                <p> Malls </p>
+        <div class="jumbotron">
+          <div class="container slider">
+            <div class="row">
+              <div class="col">
+                <a href="malls.html" />
+                <div class="malls">
+                  <img class="indexImg" src="./images/mall.jpg" />
+                  <p> Malls </p>
+                </div>
               </div>
-            </div>
-            <div class="col">
-              <a href="schools.html" />
-              <div class="schools">
-                <img class="indexImg" src="./images/school.jpg" />
-                <p> Schools</p>
+              <div class="col">
+                <a href="schools.html" />
+                <div class="schools">
+                  <img class="indexImg" src="./images/school.jpg" />
+                  <p> Schools</p>
+                </div>
               </div>
-            </div>
-            <div class="col">
-              <a href="top10.html" />
-              <div class="Top">
-                <img class="indexImg" src="./images/top10-900x400.jpg" />
-                <p> Top 10 </p>
+              <div class="col">
+                <a href="top10.html" />
+                <div class="Top">
+                  <img class="indexImg" src="./images/top10-900x400.jpg" />
+                  <p> Top 10 </p>
+                </div>
               </div>
             </div>
           </div>
+          <br></br>
+          <br></br>
         </div>
-        <br></br>
-        <br></br>
       </div>
-    </div>
-  );
+    );
+  }
 }
